8a158c1f99ad37635ff6a859dbeb4e7d1d959208
vraj-parmar <vraj170903@outlook.com>
2024-08-11 17:38:55 +0100

    Testing connection with db

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ from st_supabase_connection import SupabaseConnection
 
 # Initialize connection.
 conn = st.connection("supabase",type=SupabaseConnection)
+
+# Example usage
+if conn.is_connected():
+    data = conn.get_table("users").select("*").execute()
+    st.write(data)
+else:
+    st.error("Failed to connect to Supabase.")
