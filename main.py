a4668f760cff3ac7e9297eedb9ba324dc725dc12
vraj-parmar <vraj170903@outlook.com>
2024-08-11 17:41:48 +0100

    Testing connection with db with another method

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import streamlit as st
-from st_supabase_connection import SupabaseConnection
+from supabase import create_client, Client
 
-# Initialize connection.
-conn = st.connection("supabase",type=SupabaseConnection)
+# Initialize connection to Supabase
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
-# Example usage
-if conn.is_connected():
-    data = conn.get_table("users").select("*").execute()
-    st.write(data)
-else:
-    st.error("Failed to connect to Supabase.")
+# Create a client to interact with Supabase
+def init_supabase():
+    return create_client(SUPABASE_URL, SUPABASE_KEY)
+
+conn = init_supabase()
+
+# Use the connection
+data = conn.table("users").select("*").execute()
+
+st.write(data)
